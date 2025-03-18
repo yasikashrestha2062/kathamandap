ee2602b3a84bac7ed364abe9d4c6460ae315bdd7
Yasika <shresthayasika@gmail.com>
2025-03-18 12:51:52 +0545

    new update

--- FAQ.php
+++ FAQ.php
@@ -1,2 +1,69 @@
 <?php include('includes/header.php') ?>
+<section class="faq-banner">
+    <h3>Faqs</h3>
+    <div class="container">
+    <div class="accordion" id="accordionExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingOne">
+      <button class="accordion-button text collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+      What is Kasthamandap Educational Consultancy?
+</button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+         Kasthamandap Educational Consultancy is a professional education consultancy that provides guidance and support to students who are interested in pursuing higher education abroad.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingTwo">
+      <button class="accordion-button text collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+      What services does Kasthamandap Educational Consultancy offer?
+      </button>
+    </h2>
+    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+         Kasthamandap Educational Consultancy offers a wide range of services including university selection, admissions counseling, visa assistance, test preparation, and accommodation search.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingThree">
+      <button class="accordion-button text collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+      How can I get in touch with Kasthamandap Educational Consultancy?
+      </button>
+    </h2>
+    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+         You can get in touch with Kasthamandap Educational Consultancy by visiting their office, calling their hotline, sending an email, or messaging them on social media.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingThree">
+      <button class="accordion-button text collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+      Is Kasthamandap Educational Consultancy registered with the government?
+     </button>
+    </h2>
+    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+         Yes, Kasthamandap Educational Consultancy is registered with the government and operates by all relevant laws and regulations.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingThree">
+      <button class="accordion-button text collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">  
+      How much does it cost to work with Kasthamandap Educational Consultancy?
+      </button>
+    </h2>
+    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+         The cost of working with Kasthamandap Educational Consultancy varies depending on the services you require. They offer flexible and customizable packages to suit your specific needs and budget.
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</section>
 <?php include('includes/footer.php') ?>
--- internal-blog1.php
+++ internal-blog1.php
@@ -2,14 +2,141 @@
 
 <section class=" main-hero-banner">
 
-    <div class="blog-1">
-
-    </div>
-
-<div class="bannner-wrapper container">
-<img src="./images/blog-1.webp" alt="">
+    <div class="blog-1 ">
+    <div class="bannner-wrapper container">
+<h2 class="text-white">
+    5 ways to improve yourself
+</h2>
+<div>
+<nav style="--bs-breadcrumb-divider: '/';" aria-label="breadcrumb">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item text-white"><a href="http://localhost/kasthamandap/index.php#">Home</a></li>
+    <li class="breadcrumb-item text-white"><a href="http://localhost/kasthamandap/blog.php">Blogs</a></li>
+    <li class="breadcrumb-item text-white"><a href="#">5 ways to improve yourself</a></li>
+    
+  </ol>
+</nav>
 </div>
 
+    
+    <div class="media-wrapper">
+    <img src="./images/blog-1.webp" alt="">
+    </div>
+
+</div>
+    </div>
+
+
+
 </section>
-<section class="py-5 trial"></section>
+<section class=" trial py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-sm-12">
+<div class="blog-content-card ">
+<div class="blog-detail  p-4">
+    <p>
+        <strong>5 ways to improve yourself</strong>
+    </p>
+<p>Studying in Australia will provide you with numerous educational and personal benefits where you can gain experiences 
+    and skills that employers from all over the world will be looking for.</p>
+<p>It is an emerging market and a great place to put yourself at to boost your career and life. In today’s world, the competition in the job market is getting tougher with increasing 
+    emphasis for modern skills and a global understanding of the market.</p>
+<p>But you can improve your employability chances by planning
+     early and placing yourself ahead of the competition while studying.</p>
+<p>It is wise to seek for opportunities to gain more skills, experience, and expert guidance to make yourself marketable to potential employers.
+     Here are 5 ways to improve your career prospects while studying in Australia:</p>
+
+
+
+<p><strong>1. Working Part-time:</strong></p>
+<p>As an international student, you can work 40 hours per fortnight in Australia.</p>
+<p>Besides earning some money, part-time work will also provide you an opportunity 
+    to know about Australian work culture and learn skills in the meantime.</p>
+<p>You will also learn to take more responsibilities, develop time management 
+    skills while working, become independent and make your own decisions.</p>
+<p>Part time jobs may include roles where you interact with people of different 
+    personalities and handle clients on case to case basis as well.</p>
+<p>These experiences will help you become more confident personally and professionally.</p>
+
+
+
+
+<p><strong>2. Internship:</strong></p>
+<p>Internships will help you make a good transition between your education and career and will speed up the process of achieving your career goal.</p>
+<p>You learn a lot in your course but it can be difficult to understand important concepts and ideas until you apply the knowledge you learned which will in turn help you develop the necessary skills for the roles desired.</p>
+<p>During the internship, you will gain hands-on professional experience about the subject that you learned and that will be valuable in your chosen career.</p>
+<p>Furthermore, most of the degrees will have a range of career options and internship facilitates you to learn about different types of career roles available before you decide on the one that fits you the most.</p>
+<p>Having industry experience while graduating will give you a better edge in the competition. You may also be provided with permanent placement in the same company after your studies if you perform very well during your internship period.</p>
+
+
+
+<p><strong>3. Connections:</strong></p>
+<p>Studying in Australia will give you an opportunity to make friends and know people from all across the world.</p>
+<p>The social and professional connection with people from different backgrounds and countries will help you throughout your career and life. You will meet many peoples in your college, during your part-time work, and during your internships.</p>
+<p>No matter what roles or what professional background a person is from,
+     you will always be benefitted from the connections that you make in some part of your life.</p>
+<p>More the connections you build, the more you will get noticed and get recommendations which can prove to be
+     a great advantage you will have to become successful in your career as compared to your competitors.</p>
+<p>More the connections you build, the more you will get noticed and get recommendations which can prove
+     to be a great advantage you will have to become successful in your career as compared to your competitors.</p>
+<p>Having international friends in your circle of network has
+     an added enriching advantage. International friends can greatly help to understand foreign culture.</p>
+<p>It helps to develop inter-cultural respect and tolerance to traditions that will be foreign and difficult to grasp.</p>
+<p>In the long run, students build a global perspective of the business
+     and the markets involved which could also help in adding value to some related projects.</p>
+
+
+
+<p><strong>4. Practice English:</strong></p>
+<p>Communication is the key to securing any job available these days in this global market. As a non-native English speaker, you may face some
+     difficulties in understanding and communicate with people from different parts of the world.</p>
+<p>So, practicing more English will help you boost your confidence to communicate with people easily and comfortably.</p>
+<p>People are outspoken in the western culture and it is
+     of high importance that you are able to express your ideas out loud effectively and professionally.</p>
+<p>Additionally, big international companies require that you have a
+     good professional English and list English proficiency as one of the main requirements to get hired.</p>
+<p>Having a fluency in spoken, and written English helps to add 
+    credibility to your profile and to your workplace when dealing with clients in the local market like Nepal.</p>
+<p>In some critical roles that require frequent interaction with people, this could potentially 
+    lead to securing more business and quality clients adding to your contribution to the organization.</p>
+<p>You will gain many other advantages
+     if you can confidently speak better English in your personal and professional environment.</p>
+
+
+
+<p><strong>5. Join Clubs and Society:</strong></p>
+<p>Joining a student club or student society in your college will present
+     you with many opportunities to improve your skills, communication and learn more about your strengths.</p>
+<p>You can learn many skills from others,
+     develop soft skills and learn to work in a team setting as well as hone your interpersonal skills.</p>
+<p>Many clubs and societies 
+    will also help you develop leadership skills and add something better to your resume. Moreover, you can meet new people, make friends, meet people of similar interests and expand your connections.</p>
+<p>These involvements, general or administrative, in the clubs and organizations can open doors
+     to possible scholarship, assistantship, internship or even future employments.</p>
+<p>Also, you will be able to make good memories and get the most out of your college apart from studying.</p>
+<p>Besides these, you will have many other ways and opportunities
+     to improve your career prospects while studying in Australia which you will figure out while studying.</p>
+<p>It is very important that you put yourself out there and actively seek for opportunities.</p>
+
+
+</div>
+
+</div>
+            </div>
+            <div class="col-md-4 col-sm-12">
+            <div class="blog-content-card ">
+                <h3 class="text-primary p-2">More Content</h3>
+                <div class="content-card">
+                    <img src="./images/blog-2.webp" alt="">
+                    <div class="content-detail">
+                        <h4>Exploring Master's in Social Work Programs in Australia</h4>
+                        <p>What is a Master of Social Work?Master of Social Work (or MSW) is a gr...</p>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+</section>
 <?php include('includes/footer.php') ?>
