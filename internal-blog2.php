0f3886e8f2b3972f850ec250993079a7d9f5e3d7
Yasika <shresthayasika@gmail.com>
2025-03-19 14:15:37 +0545

    blog-update

--- internal-blog2.php
+++ internal-blog2.php
@@ -1,3 +1,170 @@
 <?php include('includes/header.php') ?>
 
+
+<section class=" main-hero-banner">
+
+    <div class="blog-1 ">
+        <div class="bannner-wrapper container">
+            <h2 class="text-white">
+                
+            Exploring Master's in Social Work Programs in Australia
+            </h2>
+            <div>
+                <nav style="--bs-breadcrumb-divider: '/';" aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item text-white"><a href="http://localhost/kasthamandap/index.php#">Home</a></li>
+                        <li class="breadcrumb-item text-white"><a href="http://localhost/kasthamandap/blog.php">Blogs</a></li>
+                        <li class="breadcrumb-item text-white"><a href="#">
+                            Exploring Master's in Social Work Programs in Australia
+                        </a></li>
+
+                    </ol>
+                </nav>
+            </div>
+
+
+            <div class="media-wrapper">
+                <img src="./images/blog-2.webp" alt="">
+            </div>
+
+        </div>
+    </div>
+
+
+
+</section>
+<section class=" trial py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-sm-12">
+                <div class="blog-content-card ">
+                    <div class="blog-detail  p-4">
+                        <p>
+                            <strong>What is a Master of Social Work?</strong>
+                        </p>
+                        <p>Master of Social Work (or MSW) is a graduate degree that equips social workers to apply for state licensure. Clinical social work is the main focus of MSW programmes, which can be completed online or with advanced standing to best suit the needs of students.</p>
+
+
+
+                        <p><strong>Why study Master's in Social Work in Australia?</strong></p>
+                        <p>Did you know that Australian social work graduates are regarded as skilled professionals in the country? The Temporary Graduate Visa (subclass 485), the Skilled Independent Visa (subclass 189), and the Skilled Nominated Visa (subclass 190), can all provide a path to permanent residency, thus boosting the possibilities of overseas students obtaining permanent residency in Australia.</p>
+
+<ul>
+    <li>Social work is among the 10 fastest-growing areas of employment.</li>
+    <li>The combination of coursework units and high-quality placements in the field presents you with the opportunity to integrate knowledge and skills and develop a sense of identity as a practitioner.</li>
+    <li>MSW is taught by highly qualified, award-winning staff in Australian universities who are passionate about teaching and learning.</li>
+    <li>Social workers promote change at the individual, community, and policy levels and assist people to create positive outcomes.</li>
+</ul>
+
+
+                        
+                    </div>
+
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-12">
+                <div class="sticky">
+                <div class="blog-content-card ">
+                    <h3>More Content</h3>
+                    <div class="content-card">
+                        <img src="./images/blog-1.webp" alt="">
+                        <div class="content-detail">
+                            <h4><a href="http://localhost/kasthamandap/internal-blog1.php">5 ways to improve yourself</a></h4>
+                            <p>5 ways to improve yourselfStudying in Australia will provide you with ...</p>
+                        </div>
+                    </div>
+
+                    <div class="content-card">
+                        <img src="./images/blog-3.jpg" alt="">
+                        <div class="content-detail">
+                            <h4>Best Countries to study and work for Nepali students</h4>
+                            <p>Studying and working abroad can be a life-changing experience, and man...</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="blog-country-card ">
+
+                    <h3> Our Country</h3>
+                    <div class="single-countries-list d-flex align-items-baseline gap-3 ">
+                        <a class="single-countries-list d-flex align-items-baseline gap-3" href="http://localhost/kasthamandap/CANADA.PHP">
+                            <i class="fa-solid fa-chevron-right"></i>
+                            <h6>Canada</h6>
+                        </a>
+                    </div>
+                    <div class="single-countries-list d-flex align-items-baseline gap-3 ">
+                        <a class="single-countries-list d-flex align-items-baseline gap-3" href="http://localhost/kasthamandap/AUSTRALIA.PHP">
+                            <i class="fa-solid fa-chevron-right"></i>
+                            <h6>Australia</h6>
+                        </a>
+                    </div>
+                    <div class="single-countries-list d-flex align-items-baseline gap-3 ">
+                        <a class="single-countries-list d-flex align-items-baseline gap-3" href="http://localhost/kasthamandap/NEWZEALAND.PHP">
+                            <i class="fa-solid fa-chevron-right"></i>
+                            <h6>New Zealand</h6>
+                        </a>
+                    </div>
+                    <div class="single-countries-list d-flex align-items-baseline gap-3 ">
+                        <a class="single-countries-list d-flex align-items-baseline gap-3" href="http://localhost/kasthamandap/UK.PHP">
+                            <i class="fa-solid fa-chevron-right"></i>
+                            <h6>United Kingdom</h6>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="blog-country-card ">
+                    <h3> Our Courses</h3>
+                    <div class="single-countries-list d-flex align-items-baseline gap-3 ">
+                        <a class="single-countries-list d-flex align-items-baseline gap-3" href="http://localhost/kasthamandap/PTE.php">
+                            <i class="fa-solid fa-chevron-right"></i>
+                            <h6>PTE</h6>
+                        </a>
+                    </div>
+                    <div class="single-countries-list d-flex align-items-baseline gap-3 ">
+                        <a class="single-countries-list d-flex align-items-baseline gap-3" href="http://localhost/kasthamandap/IELTS.php">
+                            <i class="fa-solid fa-chevron-right"></i>
+                            <h6>IELTS</h6>
+                        </a>
+                    </div>
+                    <div class="single-countries-list d-flex align-items-baseline gap-3 ">
+                        <a class="single-countries-list d-flex align-items-baseline gap-3" href="http://localhost/kasthamandap/TOFEL.php">
+                            <i class="fa-solid fa-chevron-right"></i>
+                            <h6>TOFEL</h6>
+                        </a>
+                    </div>
+                    
+                </div>
+
+                <div class="blog-country-card ">
+                    <h3> Our Services</h3>
+                    <div class="single-countries-list d-flex align-items-baseline gap-3 ">
+                        <a class="single-countries-list d-flex align-items-baseline gap-3" href="http://localhost/kasthamandap/SKILL.php">
+                            <i class="fa-solid fa-chevron-right"></i>
+                            <h6>Skill Assessment</h6>
+                        </a>
+                    </div>
+                    <div class="single-countries-list d-flex align-items-baseline gap-3 ">
+                        <a class="single-countries-list d-flex align-items-baseline gap-3" href="http://localhost/kasthamandap/CAREER.php">
+                            <i class="fa-solid fa-chevron-right"></i>
+                            <h6>Career Development Programs</h6>
+                        </a>
+                    </div>
+                    <div class="single-countries-list d-flex align-items-baseline gap-3 ">
+                        <a class="single-countries-list d-flex align-items-baseline gap-3" href="http://localhost/kasthamandap/PROFESSION.php">
+                            <i class="fa-solid fa-chevron-right"></i>
+                            <h6>Professional Year</h6>
+                        </a>
+                    </div>
+                    <div class="single-countries-list d-flex align-items-baseline gap-3 ">
+                        <a class="single-countries-list d-flex align-items-baseline gap-3" href="http://localhost/kasthamandap/EDUCATION.php">
+                            <i class="fa-solid fa-chevron-right"></i>
+                            <h6>Education</h6>
+                        </a>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 <?php include('includes/footer.php') ?>
